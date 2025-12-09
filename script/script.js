a250a6be5943fb919773e1628c4bc43070a7ca94
marki <homoki.mark@ckik.hu>
2025-12-09 12:32:36 +0100

    simogatas

--- script/script.js
+++ script/script.js
@@ -3,7 +3,8 @@ const playButton = document.getElementById("play");
 const moves = document.getElementById("moves");
 const score = document.getElementById("score");
 
-
+const rowMax = 8;
+const cellMax = 10;
 
 
 
@@ -15,9 +16,9 @@ function randomNumber(min, max) {
 
 
 function generateTable() {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < cellMax; i++) {
         let tr = document.createElement("tr")
-        for (let j = 0; j < 8; j++) {
+        for (let j = 0; j < rowMax; j++) {
             let td = document.createElement("td")
             let picture = document.createElement("img")
             if (randomNumber(0,100) <= 5) {
@@ -40,8 +41,15 @@ document.addEventListener("click", (e) =>(
     console.log(e.target)
 ))
 
+let currentRow = 0;
+let currentCell = 0;
+
 function addSanta(e){
+
     const tempTd = e.target;
+    
+
+
     if (tempTd.closest(".milk") || tempTd.closest(".cookie")) {
         return;
     }
@@ -50,11 +58,54 @@ function addSanta(e){
         return;
     }
 
+    currentRow = Number(tempTd.parentNode.rowIndex)
+    currentCell = Number(tempTd.cellIndex)
+    
+
     const img = document.createElement("img");
     img.classList.add("santa");
     img.src = "images/santa.png"
     tempTd.appendChild(img);
 }
+
+
+document.addEventListener("keyup", (e) =>{
+    if (e.key === "ArrowUp") {
+        table.rows[currentRow].cells[currentCell].innerText = "";
+        --currentRow;
+        const img = document.createElement("img");
+        img.classList.add("santa");
+        img.src = "images/santa.png"
+        table.rows[currentRow].cells[currentCell].appendChild(img);
+    }
+    if (e.key === "ArrowLeft") {
+        table.rows[currentRow].cells[currentCell].innerText = "";
+        --currentCell;
+        const img = document.createElement("img");
+        img.classList.add("santa");
+        img.src = "images/santa.png"
+        table.rows[currentRow].cells[currentCell].appendChild(img);
+    }
+    if (e.key === "ArrowRight") {
+        table.rows[currentRow].cells[currentCell].innerText = "";
+        ++currentCell;
+        const img = document.createElement("img");
+        img.classList.add("santa");
+        img.src = "images/santa.png"
+        table.rows[currentRow].cells[currentCell].appendChild(img);
+    }
+    if (e.key === "ArrowDown") {
+        table.rows[currentRow].cells[currentCell].innerText = "";
+        ++currentRow;
+        const img = document.createElement("img");
+        img.classList.add("santa");
+        img.src = "images/santa.png"
+        table.rows[currentRow].cells[currentCell].appendChild(img);
+    }
+})
+
+
+
 
 table.addEventListener("click",addSanta)
 
